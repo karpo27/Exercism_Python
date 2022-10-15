cb01d0aa25f40180b3e4a29a05dd9b36768f5519
Julian Giudice <54405665+karpo27@users.noreply.github.com>
2022-10-15 17:44:34 -0300

    Create Solution.py

---
+++ 3- Bools/3- Triangle/Solution.py
@@ -0,0 +1,35 @@
+def equilateral(sides):
+    if sides[0] > 0 and sides[1] > 0 and sides[2] > 0:
+        if sides[0] == sides[1] and sides[0] == sides[2]:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def isosceles(sides):
+    if sides[0] > 0 and sides[1] > 0 and sides[2] > 0:
+        if sides[0] == sides[1] and sides[0] == sides[2]:
+            return True
+        elif sides[0] == sides[1] and sides[0] + sides[1] >= sides[2]:
+            return True
+        elif sides[0] == sides[2] and sides[0] + sides[2] >= sides[1]:
+            return True
+        elif sides[1] == sides[2] and sides[1] + sides[2] >= sides[0]:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def scalene(sides):
+    if sides[0] > 0 and sides[1] > 0 and sides[2] > 0:
+        if sides[0] != sides[1] and sides[0] != sides[2] and sides[1] != sides[2]:
+            if sides[0] + sides[1] >= sides[2] and sides[0] + sides[2] >= sides[1] and sides[1] + sides[2] >= sides[0]:
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
