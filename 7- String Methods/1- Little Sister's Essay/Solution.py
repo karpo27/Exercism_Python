522ba71d6a4eb9951b2b4de676ed1592304ce77c
Julian Giudice <54405665+karpo27@users.noreply.github.com>
2022-10-30 19:53:09 -0300

    Update Solution.py

--- 7- String Methods/1- Little Sister's Essay/Solution.py
+++ 7- String Methods/1- Little Sister's Essay/Solution.py
@@ -1,5 +1,3 @@
-"""Functions to help edit essay homework using string manipulation."""
-
 def capitalize_title(title):
     """Convert the first letter of each word in the title to uppercase if needed.
 
