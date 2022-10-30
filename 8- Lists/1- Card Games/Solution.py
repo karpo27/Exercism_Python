9ffef1d5d85281e1c3aee48ce39fc21c1175839d
Julian Giudice <54405665+karpo27@users.noreply.github.com>
2022-10-30 19:52:41 -0300

    Create Solution.py

---
+++ 8- Lists/1- Card Games/Solution.py
@@ -0,0 +1,80 @@
+def get_rounds(number):
+    """Create a list containing the current and next two round numbers.
+
+    :param number: int - current round number.
+    :return: list - current round and the two that follow.
+    """
+    return list(range(number, number + 3))
+    
+def concatenate_rounds(rounds_1, rounds_2):
+    """Concatenate two lists of round numbers.
+
+    :param rounds_1: list - first rounds played.
+    :param rounds_2: list - second set of rounds played.
+    :return: list - all rounds played.
+    """
+    return rounds_1 + rounds_2
+
+def list_contains_round(rounds, number):
+    """Check if the list of rounds contains the specified number.
+
+    :param rounds: list - rounds played.
+    :param number: int - round number.
+    :return: bool - was the round played?
+    """
+    if number in rounds:
+        return True
+    else:
+        return False
+
+def card_average(hand):
+    """Calculate and returns the average card value from the list.
+
+    :param hand: list - cards in hand.
+    :return: float - average value of the cards in the hand.
+    """
+    return sum(hand) / len(hand)
+
+def approx_average_is_average(hand):
+    """Return if an average is using (first + last index values ) OR ('middle' card) == calculated average.
+
+    :param hand: list - cards in hand.
+    :return: bool - does one of the approximate averages equal the `true average`?
+    """
+    real_average = sum(hand) / len(hand)
+    way_1 = (hand[0] + hand[len(hand) - 1]) / 2
+    way_2 = hand[int(len(hand) / 2)]
+ 
+    if real_average == way_1 or real_average == way_2:
+        return True
+    else:
+        return False
+
+def average_even_is_average_odd(hand):
+    """Return if the (average of even indexed card values) == (average of odd indexed card values).
+
+    :param hand: list - cards in hand.
+    :return: bool - are even and odd averages equal?
+    """
+    real_average = sum(hand) / len(hand)
+    if real_average in hand:
+        if hand[int(len(hand) / 2)] == real_average:
+            return True
+        else:
+            return False
+    else:
+        return False
+    
+def maybe_double_last(hand):
+    """Multiply a Jack card value in the last index position by 2.
+
+    :param hand: list - cards in hand.
+    :return: list - hand with Jacks (if present) value doubled.
+    """
+    if hand[len(hand) - 1] == 11:
+        hand.remove(hand[len(hand) - 1])
+        hand.append(22)
+        return hand
+    else:
+        return hand
+        
