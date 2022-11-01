83ad32dc8747439e6e430db52df4c9f21c817650
Julian Giudice <54405665+karpo27@users.noreply.github.com>
2022-11-01 19:41:20 -0300

    Create Solution.py

---
+++ 9- List Methods/3- Secret Handshake/Solution.py
@@ -0,0 +1,18 @@
+def commands(binary_str):
+
+    handshake = []
+    for i in range(0, len(binary_str)):
+        if binary_str[1] == "1":
+            handshake.append("jump")
+        if binary_str[2] == "1":
+            handshake.append("close your eyes")
+        if binary_str[3] == "1":
+            handshake.append("double blink")
+        if binary_str[4] == "1":
+            handshake.append("wink")
+
+        if binary_str[0] == "1":
+            return handshake
+        else:
+            return list(reversed(handshake))
+            
