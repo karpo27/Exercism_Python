68f93384dc36d5e8955b494c90844b834bd6433a
Julian Giudice <54405665+karpo27@users.noreply.github.com>
2023-06-26 12:24:02 -0300

    Create Solution.py

---
+++ 07 String Methods/05 Hamming/Solution.py
@@ -0,0 +1,10 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+
+    hamming = 0
+    for letter_a, letter_b in zip(strand_a, strand_b):
+        if letter_a != letter_b:
+            hamming += 1
+
+    return hamming
