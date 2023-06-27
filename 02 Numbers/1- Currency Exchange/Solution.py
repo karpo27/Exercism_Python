01cab87d161f7075b32342cc799a662d1a9d942e
Julian Giudice <54405665+karpo27@users.noreply.github.com>
2023-06-27 17:30:34 -0300

    Update Solution.py

--- 02 Numbers/1- Currency Exchange/Solution.py
+++ 02 Numbers/1- Currency Exchange/Solution.py
@@ -33,3 +33,26 @@ def get_number_of_bills(budget, denomination):
     """
 
     return int(budget // denomination)
+
+def get_leftover_of_bills(budget, denomination):
+    """
+    :param budget: float - the amount of money you are planning to exchange.
+    :param denomination: int - the value of a single bill.
+    :return: float - the leftover amount that cannot be exchanged given the current denomination.
+    """
+
+    return budget % denomination
+
+def exchangeable_value(budget, exchange_rate, spread, denomination):
+    """
+    :param budget: float - the amount of your money you are planning to exchange.
+    :param exchange_rate: float - the unit value of the foreign currency.
+    :param spread: int - percentage that is taken as an exchange fee.
+    :param denomination: int - the value of a single bill.
+    :return: int - maximum value you can get.
+    """
+    
+    num_1 = budget / (exchange_rate + (exchange_rate * (spread / 100)))
+    num_2 = num_1 // denomination
+    num_3 = num_2 * denomination
+    return num_3
